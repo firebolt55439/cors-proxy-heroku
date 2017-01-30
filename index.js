8510a72aa044691510c6e494437a3a11b5714a52
firebolt55439 <sumer112@gmail.com>
2017-01-29 19:30:42 -0800

    Fixed port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var http = require('http'),
 var index = fsRead('index.html');
 var indexGzip = gzip(index);
 
-var port = process.env.PORT || 8080;
+var port = 80;
 var debug = process.argv.indexOf('--debug') != -1;
 
 var errorString = chalk.red;
